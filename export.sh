46750317be149a49c4d854ca86d23555cd6b8d8e
sagnikmjr <sagnik@cs.utexas.edu>
2019-11-23 14:22:18 -0600

    export.sh for convenience, run it before executing python ...

---
+++ export.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+export RL_DATA=/home/sagnik/Documents/code_n_data/repos/personal/RL_standalone/data
+export REPO_DIR=/home/sagnik/Documents/code_n_data/repos/personal/RL_standalone/wge
+export RL_DEMO_DIR=$REPO_DIR/third-party/miniwob-demos/
+export MINIWOB_BASE_URL=file:///home/sagnik/Documents/code_n_data/repos/personal/RL_standalone/wge/miniwob-sandbox/html/
